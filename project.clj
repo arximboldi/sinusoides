b534f4de10d81a1569219a348ddc237baaca95de
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2015-02-20 00:46:09 +0100

    Upgrade sablono to latest

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljs-http "0.1.24"]
                  [com.andrewmcveigh/cljs-time "0.3.0"]
-                 [sablono "0.2.22"]]
+                 [sablono "0.3.4"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]]
   :source-paths ["src"]
@@ -22,10 +22,12 @@
        :source-paths ["src"]
        :compiler {:output-to "build/debug/sinusoides.js"
                   :output-dir "build/debug/out"
+                  :main sinusoides.core
                   :optimizations :none
                   :source-map true}}
       {:id "release"
        :source-paths ["src"]
        :compiler {:output-to "build/release/sinusoides.js"
+                  :main sinusoides.core
                   :optimizations :advanced
                   :pretty-print false}}]})
