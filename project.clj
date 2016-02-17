72569b4534d5bc95b9d5cf1b6920d69fa2d1516e
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-02-17 23:04:57 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
 
   :dependencies [[cljs-http "0.1.24"]
                  [com.andrewmcveigh/cljs-time "0.3.0"]
-                 [kibu/pushy "0.2.2"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [kibu/pushy "0.3.6"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.omcljs/om "0.8.8"]
                  [prismatic/om-tools "0.3.10"]
                  [sablono "0.3.4"]
-                 [secretary "1.2.1"]]
+                 [secretary "1.2.3"]]
 
-  :plugins [[lein-cljsbuild "1.0.4"]]
+  :plugins [[lein-cljsbuild "1.1.2"]]
   :source-paths ["src"]
 
   :cljsbuild {
