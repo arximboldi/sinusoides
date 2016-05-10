884676731db5320a4d5261f70343c5982006a96c
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2016-05-10 15:37:41 +0200

    wip - clojurescript error!

---
+++ src/deps.cljs
@@ -0,0 +1,10 @@
+{:foreign-libs
+ [{:file      "libs/fontfaceobserver/fontfaceobserver.js"
+   :file-min  "libs/fontfaceobserver/fontfaceobserver.js"
+   :provides  ["fontfaceobserver"] }
+  {:file      "libs/plyr/dist/plyr.js"
+   :file-min  "libs/plyr/dist/plyr.js"
+   :provides  ["plyr"]}]
+
+ :externs
+ ["libs/fontfaceobserver/externs.js"]}
--- src/sinusoides/views/think.cljs
+++ src/sinusoides/views/think.cljs
@@ -21,37 +21,71 @@
   (:require [sinusoides.views.sinusoid :as sinusoid]
             [sinusoides.views.addons :refer [css-transitions]]
             [sinusoides.util :as util]
+            [clojure.string :as string]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<! timeout]]
-            [reagent.core :as r]))
+            [reagent.core :as r]
+            [plyr]))
 
-(defn soundcloud-thumbnail-view [thing]
-  [:iframe.thingy.soundcloud
-   {:scrolling "no"
-    :frame-border "no"
-    :src (str "https://w.soundcloud.com/player/?"
-              "url=https%3A//api.soundcloud.com/tracks/" (:track thing) "&amp;"
-              "auto_play=false&amp;"
-              "hide_related=true&amp;"
-              "buying=false&amp;"
-              "liking=false&amp;"
-              "sharing=false&amp;"
-              "download=false&amp;"
-              "show_comments=false&amp;"
-              "show_user=true&amp;"
-              "show_reposts=false&amp;"
-              "show_playcount=false&amp;"
-              "visual=true")}])
+(def sc-client-id
+  "485230fd2a6e151244a57a584f904070")
+
+(defn sc-add-client-id [api-call]
+  (str api-call "?client_id=" sc-client-id))
+
+(defn sc-api [& command]
+  (http/jsonp (sc-add-client-id
+                (str "http://api.soundcloud.com"
+                     (apply str command)
+                     ".json"))))
+
+(defn plyr-sc-thumbnail-view [thing]
+  (let [data (r/atom {})]
+    ^{:component-did-mount
+      (fn [this]
+        (go (reset! data (:body (<! (sc-api "/tracks/" (:media-id thing)))))
+            (.setup
+              js/plyr
+              (r/dom-node this)
+              (clj->js
+                {:iconUrl "/static/pic/plyr-sprite.svg"
+                 :controls [:play :progress :current-time :duration]
+                 :fullscreen {:enabled false}}))))}
+    (fn [thing]
+      (if @data
+        (let [background (string/replace (:artwork_url @data)
+                                         #"-large.jpg"
+                                         "-t500x500.jpg")]
+          [:div.thingy.soundcloud.plyr
+           {:style {:background-image (str "url(" background ")")}}
+           [:audio [:source {:src (sc-add-client-id (:stream_url @data))
+                             :type "audio/mp3"}]]])
+        [:div]))))
+
+(def plyr-thumbnail-embed-view
+  ^{:component-did-mount
+    (fn [this]
+      (js/plyr.setup (r/dom-node this)
+        (clj->js
+          {:iconUrl "/static/pic/plyr-sprite.svg"
+           :controls [:play :progress :current-time :duration]
+           :fullscreen {:enabled false}})))}
+
+  (fn [thing]
+    [:div.thingy.plyr
+     [:div {:data-video-id (:media-id thing)
+            :data-type (:type thing)}]]))
 
 (defn text-thumbnail-view [thing]
   [:div.thingy.text (:title thing)])
 
 (def thumbnail-view-map
-  {"soundcloud" soundcloud-thumbnail-view
+  {"soundcloud" plyr-sc-thumbnail-view
    "text"       text-thumbnail-view})
 
 (defn think-view [sin think]
-  (r/with-let [_ (go (let [response (<! (http/get "/data/think.json"))
+  (r/with-let [_ (js/plyr.setup)
+               _ (go (let [response (<! (http/get "/data/think.json"))
                            entries  (map #(assoc % :slug (util/to-slug (:title %)))
                                          (:body response))]
                        (swap! think assoc-in [:entries] entries)))]
